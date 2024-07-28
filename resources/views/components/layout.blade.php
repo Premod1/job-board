e17c11032d30da2e2ceb013fd3f3455c95cebca8
premod <premodsuraweera1@gmail.com>
2024-07-28 15:54:32 +0530

    feat:job card and tag add

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,10 +10,8 @@
 
 </head>
 
-<body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+   {{ $slot }}
 </body>
 
 </html>
