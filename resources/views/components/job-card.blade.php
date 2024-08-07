990b550759b6c89c8fe40003c222d62221d4cdc8
premod <premodsuraweera1@gmail.com>
2024-08-07 17:45:20 +0530

    make radio button

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -15,8 +15,6 @@
             <x-tag>{{ $job->category}}</x-tag>
         </div>
     </div>
-    <p class="text-sm text-slate-500">
-        {!! nl2br(e($job->description)) !!}
-    </p>
+
     {{ $slot}}
 </x-card>
